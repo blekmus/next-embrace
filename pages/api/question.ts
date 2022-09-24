57dfb2e9bd93837d1793c2318724c2c2112916eb
blekmus <dinil@protonmail.com>
2022-09-24 11:07:06 +0530

    google priv key for questions

--- pages/api/question.ts
+++ pages/api/question.ts
@@ -34,7 +34,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREADSHEET_ID)
     await doc.useServiceAccountAuth({
       client_email: process.env.GOOGLE_CLIENT_EMAIL || '',
-      private_key: process.env.GOOGLE_PRIVATE_KEY || '',
+      private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n') || '',
     })
 
     await doc.loadInfo()
